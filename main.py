2d166fec5dc3703ac868a05242909bb1272433d3
Jeisson Felipe Manrique Pulido <jeissonmp15@gmail.com>
2022-11-17 13:04:55 -0500

    Conversion a hexa str

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import socket
+import os
+import binascii
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+os.system("sudo fuser -k -n tcp {}".format(10000))
 
 # Bind the socket to the port
 server_address = ('0.0.0.0', 10000)
@@ -21,13 +25,14 @@ while True:
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(16)
-            print('received {!r}'.format(data))
-            if data:
-                print('sending data back to the client')
-                connection.sendall(data)
-            else:
-                print('no data from', client_address)
-                break
+            info = str(binascii.hexlify(data).decode('iso-8859-1')).upper
+            print('Data: {!r}'.format(info))
+            # if data:
+            #     print('sending data back to the client')
+            #     connection.sendall(data)
+            # else:
+            #     print('no data from', client_address)
+            #     break
 
     finally:
         # Clean up the connection
